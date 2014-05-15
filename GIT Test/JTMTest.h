98f53c69629d1f4cb14312e466c7d8e867466f9a
John Mims <jmims@Johns-MacBook-Pro-3.local>
2014-05-14 22:03:37 -0400

    added a test class

---
+++ GIT Test/JTMTest.h
@@ -0,0 +1,13 @@
+//
+//  JTMTest.h
+//  GIT Test
+//
+//  Created by John Mims on 5/14/14.
+//  Copyright (c) 2014 Mims Comm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JTMTest : NSObject
+
+@end
---
+++ GIT Test/JTMTest.m
@@ -0,0 +1,13 @@
+//
+//  JTMTest.m
+//  GIT Test
+//
+//  Created by John Mims on 5/14/14.
+//  Copyright (c) 2014 Mims Comm. All rights reserved.
+//
+
+#import "JTMTest.h"
+
+@implementation JTMTest
+
+@end
